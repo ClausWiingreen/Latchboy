fc3276aecf6da18e011a8e6965e8bc14a862e341
Claus Wiingreen <ClausWiingreen@users.noreply.github.com>
2026-04-15 06:34:41 +0200

    Refine interrupt module and mark mechanism complete

--- core/src/interrupts.rs
+++ core/src/interrupts.rs
@@ -1,2 +1,4 @@
-#[derive(Debug, Default)]
-pub struct Stub;
+pub const FLAG_REGISTER: u16 = 0xFF0F;
+pub const ENABLE_REGISTER: u16 = 0xFFFF;
+pub const MASK: u8 = 0x1F;
+pub const VECTORS: [u16; 5] = [0x40, 0x48, 0x50, 0x58, 0x60];
