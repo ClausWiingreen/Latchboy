2f02fc3de6c85dcdc663b5b31da1dfd51b85f435
Claus Wiingreen <ClausWiingreen@users.noreply.github.com>
2026-04-29 16:58:24 +0200

    Advance PPU mode 0 transition by one M-cycle

--- core/src/ppu.rs
+++ core/src/ppu.rs
@@ -632,7 +632,7 @@ impl Ppu {
             1
         } else if self.scanline_dot < MODE2_CYCLES {
             2
-        } else if self.scanline_dot < (MODE0_CYCLES_END - 1) {
+        } else if self.scanline_dot < (MODE0_CYCLES_END - 4) {
             3
         } else {
             0
